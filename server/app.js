3778b764590835da240afe9386697ecae86e1c60
Casey Hyde <hyde.casey@gmail.com>
2017-01-31 13:44:49 -0600

    Basic folder structure set up, index.html loads, server runs on npm test and start

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const LOCALPORT = 3000;
+var portDecision = process.env.PORT || LOCALPORT;
+
+//Serve static files
+app.use(express.static('public'));
+
+//Index.html route
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+app.listen(portDecision, function() {
+  console.log("Listening on port ", portDecision);
+});
