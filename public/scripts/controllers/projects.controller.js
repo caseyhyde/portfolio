48713f5dcc5978414a783dc26db3fbcec686ce36
Casey Hyde <hyde.casey@gmail.com>
2017-02-04 14:25:22 -0600

    More changes to Projects page

--- public/scripts/controllers/projects.controller.js
+++ public/scripts/controllers/projects.controller.js
@@ -1,3 +1,11 @@
 app.controller('ProjectsController', function() {
   console.log('ProjectsController running');
+
+  const self = this;
+
+  self.openProjects = function(projectToOpen) {
+    self[projectToOpen] = true;
+  };
+
+
 });
