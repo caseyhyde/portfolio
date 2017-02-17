63e45c03090892ad05c99d0e105f5b6632fb84d7
Casey Hyde <hyde.casey@gmail.com>
2017-02-17 03:01:50 -0600

    Playing with z-indexes to keep links clickable

--- public/scripts/directives/scroll.directive.js
+++ public/scripts/directives/scroll.directive.js
@@ -1,14 +1,13 @@
 app.directive("scroll", function ($window) {
-    return function(scope, element, attrs) {
-
-        angular.element($window).bind("scroll", function() {
-            if (this.pageYOffset >= 65) {
-                 scope.boolChangeClass = true;
-             } else {
-                 scope.boolChangeClass = false;
-                 scope.$apply();
-             }
-            scope.$apply();
-        });
-    };
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() {
+      if (this.pageYOffset >= 65) {
+        scope.boolChangeClass = true;
+      } else {
+      scope.boolChangeClass = false;
+        scope.$apply();
+      }
+      scope.$apply();
+    });
+  };
 });
