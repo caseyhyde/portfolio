1ff45f8d21e416a84895d84dd562ea48facfe5eb
Casey Hyde <hyde.casey@gmail.com>
2017-03-05 21:42:26 -0600

    View change on successful message send

--- public/scripts/controllers/contact.controller.js
+++ public/scripts/controllers/contact.controller.js
@@ -30,21 +30,22 @@ app.controller('ContactController', ['$http', '$mdDialog', function($http, $mdDi
       })
     }
     else {
-        self.status = '  ';
-        self.customFullscreen = false;
-
-        self.showAlert = function(ev) {
-          $mdDialog.show(
-            $mdDialog.alert()
-            .parent(angular.element(document.querySelector('#popupContainer')))
-            .clickOutsideToClose(true)
-            .title('This is an alert title')
-            .textContent('You can specify some description text in here.')
-            .ariaLabel('Alert Dialog Demo')
-            .ok('Got it!')
-            .targetEvent(ev)
-        );
-      };
+      console.log("Nothing in input fields");
+    //     self.status = '  ';
+    //     self.customFullscreen = false;
+    //
+    //     self.showAlert = function(ev) {
+    //       $mdDialog.show(
+    //         $mdDialog.alert()
+    //         .parent(angular.element(document.querySelector('#popupContainer')))
+    //         .clickOutsideToClose(true)
+    //         .title('This is an alert title')
+    //         .textContent('You can specify some description text in here.')
+    //         .ariaLabel('Alert Dialog Demo')
+    //         .ok('Got it!')
+    //         .targetEvent(ev)
+    //     );
+    //   };
     }
   }
 
